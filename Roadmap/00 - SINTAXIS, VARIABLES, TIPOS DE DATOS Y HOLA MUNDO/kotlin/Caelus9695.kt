dac9634ff45017e6513a7127091f52cad0c609c2
Caelus9695 <davidquintana9695@gmail.com>
2025-05-14 07:00:24 -0300

    Create Caelus9695.kt

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/kotlin/Caelus9695.kt
@@ -0,0 +1,28 @@
+// EJ.1: Crea un comentario en el código y coloca la URL del sitio web oficial del lenguaje de programación que has seleccionado.
+//https://kotlinlang.org/
+
+// EJ.2: Representa las diferentes sintaxis que existen de crear comentarios en el lenguaje (en una línea, varias...).
+//Este es un comentario de una sola línea
+/*Este es un
+comentario
+de varias 
+líneas/*
+
+//EJ.3: - Crea una variable (y una constante si el lenguaje lo soporta).
+//Variables
+val estaEsUnaVariableMutable = 22
+val estaEsUnaVariableFija = 55
+//Constantes
+const val estaEsUnaConstante = "bb"
+
+//EJ.4: Crea variables representando todos los tipos de datos primitivos del lenguaje (cadenas de texto, enteros, booleanos...).
+val cadenasDeTexto: String = "Hola bb"
+val enteros: Int = 69
+val decimales: Double = 555.844
+val float: Float = 984.0F
+val booleanos: Boolean = false
+
+//EJ.5: Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+fun main() {
+    print("¡Hola, Kotlin!")
+}
